24158060755b0baf8a4c6e411430073621733b84
Lucien <lucien.lang@proton.me>
2024-12-02 19:06:41 +0100

    init 3

---
+++ 2024/3/main.py
@@ -0,0 +1,29 @@
+
+
+def getInput():
+
+    input = []
+    with open("2024/3/input.txt", "r") as file:
+        lines = file.readlines()
+
+
+    return
+
+def solve1(input):
+    pass
+
+
+
+
+def solve2(input):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    input = getInput()
+
+    print(solve1(input))
+
+    # print(solve2(input))
