66b9b0f18bf09bbe329fe99cf7e56610bec260f3
Lucien <lucien.lang@proton.me>
2024-12-03 08:22:31 +0100

    4 init

---
+++ 2024/4/main.py
@@ -0,0 +1,33 @@
+
+
+def getInput():
+
+    input = []
+    with open("2024/4/test.txt", "r") as file:
+        lines = file.readlines()
+
+
+    return 
+
+def solve1(input):
+    
+
+    return 
+
+
+
+def solve2(input):
+    
+
+    return 
+
+
+
+
+if __name__ == "__main__":
+    input = getInput()
+
+    print(solve1(input))
+    
+
+    print(solve2(input))
