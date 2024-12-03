8da382b7c223d7dae0b83aa9e78be34d03d9cdc2
Lucien <lucien.lang@proton.me>
2024-12-03 07:04:21 +0100

    1 done

--- 2024/3/main.py
+++ 2024/3/main.py
@@ -1,22 +1,44 @@
-
+import re
 
 def getInput():
 
     input = []
-    with open("2024/3/input.txt", "r") as file:
+    with open("2024/3/test.txt", "r") as file:
         lines = file.readlines()
 
 
-    return
+    return lines
 
 def solve1(input):
-    pass
+    sume = 0
+    for line in input:
+        matches = re.findall(r'mul\(\d*,\d*\)',line )
+        # print (matches)
+        for match in matches:
+            left, right = match.split(",")
+            left = re.findall(r'\d*', left)[-2]
+            right = re.findall(r'\d*', right)[0]
+            print(left,"*", right)
+            sume += int(left) * int(right)
 
+    return sume
 
 
 
 def solve2(input):
-    pass
+    sume = 0
+    for line in input:
+        line = line.sub(r'don\'t\(\).*do\(\)')
+        matches = re.findall(r'mul\(\d*,\d*\)',line )
+        print (matches)
+        for match in matches:
+            left, right = match.split(",")
+            left = re.findall(r'\d*', left)[-2]
+            right = re.findall(r'\d*', right)[0]
+            print(left,"*", right)
+            sume += int(left) * int(right)
+
+    return sume
 
 
 
@@ -24,6 +46,7 @@ def solve2(input):
 if __name__ == "__main__":
     input = getInput()
 
-    print(solve1(input))
+    # print(solve1(input))
+    # 173419328
 
-    # print(solve2(input))
+    print(solve2(input))
