bcc892714d2d2067e68ad073056a5fc3c54db833
Lucien <lucien.lang@proton.me>
2024-12-05 09:45:40 +0100

    d6init

---
+++ 2024/6/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+DAY = 6
+FILE = "test.txt"
+
+def getInput():
+
+    
+    with open(f"2024/{DAY}/{FILE}", "r") as file:
+        pass
+
+
+    return 
+
+def solve1(input):
+
+    return 
+
+
+
+def solve2(input):
+
+
+    return 
+
+
+
+
+if __name__ == "__main__":
+    input = getInput()
+    print(solve1(input))
+    print(solve2(input))
