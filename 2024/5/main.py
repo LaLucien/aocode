1800f64829319f47e467cc5c35890bcc60101616
Lucien <lucien.lang@proton.me>
2024-12-05 06:29:34 +0100

    get input

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -4,13 +4,32 @@ DAY = 5
 FILE = "test.txt"
 
 def getInput():
-
-    
     with open(f"2024/{DAY}/{FILE}", "r") as file:
-        pass
+        lines = file.readlines()
 
+    beforeOrdering = {}
+    printingInput = []
+    secondPartReached = False
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            secondPartReached = True
+            continue
 
-    return 
+        if secondPartReached:
+            num = []
+            for cnum in line.split(","):
+                num.append(cnum)
+            printingInput.append(np.array(num))
+        else:
+            left, right = int(line.split("|")[0]), int(line.split("|")[1])
+            if left in beforeOrdering.keys():
+                beforeOrdering[left].append(right)
+            else:
+                beforeOrdering[left] = [right]
+            
+
+    return beforeOrdering, printingInput
 
 def solve1(input):
 
