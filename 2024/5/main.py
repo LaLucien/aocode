67cf50be6da0d67dfa9f23a74073cb3700ff25fd
Lucien <lucien.lang@proton.me>
2024-12-05 08:27:12 +0100

    d5p2

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -36,6 +36,7 @@ def solve1(input):
     printing = input[1]
 
     validPrintsMiddleSum = 0
+    incorectlyOrder = []
     for print in printing:
         alreadyPrinted = []
         isValid = True
@@ -50,6 +51,7 @@ def solve1(input):
             for mustComeAfter in ordering[currentPage]:
                 if mustComeAfter in alreadyPrinted:
                     isValid = False
+                    incorectlyOrder.append(print)
                     break
 
             if not isValid:
@@ -66,14 +68,32 @@ def solve1(input):
 
 
 
-    return validPrintsMiddleSum
+    return validPrintsMiddleSum, incorectlyOrder
 
 
 
 def solve2(input):
+    ordering = input[0]
+    # printing = input[1]
+    dontCare, wrongPrints = solve1(input)
 
-
-    return 
+    middleSum = 0
+    # insertion sort
+    for print in wrongPrints:
+        i = 1
+        while i < len(print):
+            j = i
+            # inserts elements that have no rules at end, inserts other where to be sorted page doesnt need to be after.
+            while j > 0 and print[j] in ordering.keys() and print[j-1] in ordering[print[j]]:
+                tmp = print[j]
+                print[j] = print[j-1]
+                print[j-1] = tmp
+                j -=1
+            i += 1
+    
+        middleSum += print[int(len(print)/2)]
+            
+    return middleSum
 
 
 
