05a8664ee9d596a4f610b67f3000c86fea3561f7
Lucien <lucien.lang@proton.me>
2024-12-09 16:02:27 +0100

    not yet doen

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -1,20 +1,54 @@
 import numpy as np
+from numba import jit, njit, prange
 
-DAY = -1
+DAY = 7
 FILE = "test.txt"
 
 def getInput():
 
-    
+    solutions = []
+    equations = []
     with open(f"2024/{DAY}/{FILE}", "r") as file:
-        pass
+        lines = file.readlines()
+
+    for line in lines:
+        solutions.append(int(line.split(":")[0]))
+        numbers = []
+        for snum in line.strip().split(": ")[1].split(" "):
+            numbers.append(int(snum))
+        equations.append(np.array(numbers))
+    
+    return (np.array(solutions), equations)
 
 
-    return 
+def countPossibilities(sol, numbers, currentValue, currentIndex):
+    # correct
+    if (sol == currentValue and currentIndex >= len(numbers) - 1):
+        return 1
+    #overshoot, no more numbers
+    if(sol < currentValue or currentIndex >=  len(numbers) - 1):
+        return 0
+    
+
+    foundPossibilities = countPossibilities(sol, numbers, numbers[currentIndex] + numbers[currentIndex + 1], currentIndex + 1)
+    return   foundPossibilities + countPossibilities(sol, numbers, numbers[currentIndex] * numbers[currentIndex + 1], currentIndex + 1) 
+    
+
+    
+
 
 def solve1(input):
+    solutions = input[0]
+    equations = input[1]
+    # operators = np.array(["+", "*"])
 
-    return 
+    rightPossibilities = 0
+
+    for i in prange(len(solutions)):
+        rightPossibilities += countPossibilities(solutions[i], equations[i],   equations[i][0] + equations[i][0 + 1], 1)
+        rightPossibilities += countPossibilities(solutions[i], equations[i],   equations[i][0] * equations[i][0 + 1], 1)
+
+    return rightPossibilities
 
 
 
